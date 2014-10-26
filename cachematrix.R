4c8198d55e998315accf8c08c9e461e718e46e79
dfpeterson <dfpeterson@gmail.com>
2014-10-26 18:58:51 -0400

    Submission
    Completed functions for grading submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
+## This function creates a matrix that can return the inverse
+## of a squarable matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(mean) m <<- solve
+  getinverse <- function() solve(x)
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)  
 }
-
-
-## Write a short comment describing this function
-
+## cacheSolve returns a cached inverse of the matrix
+## if one is available
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
+  
+  ## Return a matrix that is the inverse of 'x'
 }
